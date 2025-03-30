b992b1d98933bd9879674bd900f9b7a8eb3ccb3a
sakhura <sabina.romero@gmail.com>
2025-03-29 23:00:18 -0300

    eventos

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -9,12 +9,28 @@ import { CommonModule } from '@angular/common';
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
+ //propiedades atributos 
   welcome = 'Bienvenido a su primera aplicación en Angular';
-  task =[
+  task = [
     'Instalar Angular Cli',
     'Crear Poryecto',
     'Crear Component',
     'Crear Servicio',
   ];
+  name = 'Sabina';
+  age = 50;
+  disable = true;
+  img = 'https://cdnx.jumpseller.com/shazam-online/image/45040948/kaiju_08_08.jpg?1706550475'
 
+
+  person = {
+    name: 'Sabina',
+    age: 50,
+    avatar: 'https://cdnx.jumpseller.com/shazam-online/image/45040948/kaiju_08_08.jpg?1706550475'
+  }
+
+  // metodos 
+  clickHandler(){
+    alert  ('alerta')
+  }
 }
